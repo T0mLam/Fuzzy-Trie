4429ae4d1ad332b39e25484117a2f353b871bd5e
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-06-13 23:41:11 +0100

    update docstr

--- py_trie/fuzzy_trie.py
+++ py_trie/fuzzy_trie.py
@@ -16,4 +16,44 @@ class FuzzyTrie(Trie):
         fizzy_search: Search a list of words within a Levenshtein distance to the target string in the trie.
     """
     
-    def __in
+    def __init__(self) -> None:
+        """Inherit the attributes and methods from the base Trie."""
+        super().__init__()
+
+    def fuzzy_search(
+        target: str, 
+        threshold: int,
+        sort_by_distance: bool = False,
+        num_return: int | None = None
+    ) -> List[str]:
+        """An approximate string matching method that return a list of word within a Levenshtein distance threshold.
+        
+        Args:
+            target (str): The target word.
+            threshold (int): The maximum Levenshtein distance difference.
+            sort_by_distance (bool): Return the words in ascending order of LD difference. (Default=False)
+            num_return (int): The maximum number of return words. (Default=None)
+
+        Returns:
+            List[str]:  A list of words that are within the Levenshtein distance threshold in the trie.
+
+        Raises:
+            TypeError: Invalid data type of input parameter 'word'.
+            ValueError: Invalid data type and range of input parameters 'threshold' or 'num_return'.
+
+        Notes:
+            Additional time complexity is introduced if 'sort_by_distance' is set to True.
+        """
+        if not isinstance(target, str):
+            raise TypeError("The input parameter 'target' must be a string.")
+        
+        if not isinstance(sort_by_distance, bool):
+            raise TypeError("The input parameter 'sort_by_distance' must be a boolean.")
+        
+        if not isinstance(threshold, int) or threshold < 0:
+            raise ValueError("The input parameter 'threshold' must be a non-negative integer")
+        
+        if (not isinstance(num_return, None) and
+            (not isinstance(num_return, int) or num_return < 1)):
+            raise ValueError("The input parameter 'num_return' must be a positive integer")
+        
