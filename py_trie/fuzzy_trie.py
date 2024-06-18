aefa3c26ffe866ccced39bb4912912ade1e8a581
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-06-18 22:51:16 +0100

    update fuzzy_search

--- py_trie/fuzzy_trie.py
+++ py_trie/fuzzy_trie.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from .node import TrieNode
 from .trie import Trie
 
 
@@ -21,6 +22,7 @@ class FuzzyTrie(Trie):
         super().__init__()
 
     def fuzzy_search(
+        self,
         target: str, 
         threshold: int,
         sort_by_distance: bool = False,
@@ -53,7 +55,68 @@ class FuzzyTrie(Trie):
         if not isinstance(threshold, int) or threshold < 0:
             raise ValueError("The input parameter 'threshold' must be a non-negative integer")
         
-        if (not isinstance(num_return, None) and
+        if (not isinstance(num_return, type(None)) and
             (not isinstance(num_return, int) or num_return < 1)):
             raise ValueError("The input parameter 'num_return' must be a positive integer")
         
+        cols = len(target) + 1
+        res = []
+        
+        def dfs(
+            node: TrieNode, 
+            curr_str: str,
+            prev_row: List[int]
+        ) -> None:
+            # Get the latest added character from the string
+            letter = curr_str[-1]
+
+            # Get the new row using the edit distance algorithm
+            curr_row = edit_dist(letter, prev_row)
+
+            # If the last value of the row is less than the threshold
+            # and it is a word stored in the trie, push the word
+            # along with the Levenshtein distance to the result  
+            if curr_row[-1] <= threshold and node.end_of_word:
+                res.append((curr_str, curr_row[-1]))
+
+            # If the minimum value of the row has not exceeded the threshold, 
+            # it is possible that adding additional characters 
+            # to the end of the word will still be valid
+            if min(curr_row) <= threshold:
+                for child in node.children:
+                    dfs(node.children[child], curr_str + child, curr_row)
+
+        def edit_dist(
+            letter: str,
+            prev_row: List[int]
+        ) -> int:
+            # Create the first value of the new row
+            curr_row = [prev_row[0] + 1]
+
+            for col in range(1, cols):
+                # Add the value of the top left grid to curr_row 
+                # if the combination and target share the same letter 
+                if target[col - 1] == letter:
+                    curr_row.append(prev_row[col - 1])
+                    continue
+
+                # Otherwise calculate each cost of the 3 grids at the 3 directions
+                # according to the edit distance algorithm
+                replace_cost = prev_row[col - 1] + 1
+                insert_cost = curr_row[col - 1] + 1
+                delete_cost = prev_row[col] + 1
+
+                # Append the minimum cost to the curr_row
+                curr_row.append(min(replace_cost, insert_cost, delete_cost))
+
+            return curr_row
+    
+        node = self.root
+        # Create the first row of the 2d matrix
+        first_row = range(len(target) + 1)
+
+        # Start the recursion from every child of the trie root
+        for child in node.children:
+            dfs(node.children[child], child, first_row)
+
+        return res
