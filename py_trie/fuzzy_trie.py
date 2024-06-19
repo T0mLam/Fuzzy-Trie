5c987a8dc22201e0c56a49f5c73098a9ab3643fb
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-06-19 21:39:32 +0100

    update comments

--- py_trie/fuzzy_trie.py
+++ py_trie/fuzzy_trie.py
@@ -13,8 +13,11 @@ class FuzzyTrie(Trie):
     Methods:
         insert: Insert a string into the Trie.
         find: Return True if the word is in the Trie else False.
-        complete: Complete a word based on the input of the user and return the list of words ordered by their length.
-        fizzy_search: Search a list of words within a Levenshtein distance to the target string in the trie.
+        delete: Delete a word in the Trie and return True if successful.
+        complete: Complete a word based on the input of the user and
+                  return the list of words ordered by their length.
+        fizzy_search: Search a list of words within a Levenshtein distance
+                      to the target string in the trie.
     """
     
     def __init__(self) -> None:
@@ -28,23 +31,28 @@ class FuzzyTrie(Trie):
         num_return: int | None = None,
         sort_by_distance: bool = False
     ) -> List[str]:
-        """An approximate string matching method that return a list of word within a Levenshtein distance threshold.
+        """An approximate string matching method that return a list of word 
+        within a Levenshtein distance threshold.
         
         Args:
             target (str): The target word.
             threshold (int): The maximum Levenshtein distance difference.
-            sort_by_distance (bool): Return the words in ascending order of LD difference. (Default=False)
+            sort_by_distance (bool): Return the words in ascending order of 
+                                     LD difference. (Default=False)
             num_return (int): The maximum number of return words. (Default=None)
 
         Returns:
-            List[str]:  A list of words that are within the Levenshtein distance threshold in the trie.
+            List[str]:  A list of words that are within the Levenshtein distance 
+                        threshold in the trie.
 
         Raises:
             TypeError: Invalid data type of input parameter 'word'.
-            ValueError: Invalid data type and range of input parameters 'threshold' or 'num_return'.
+            ValueError: Invalid data type and range of input parameters
+                        'threshold' or 'num_return'.
 
         Notes:
-            Additional time complexity is introduced if 'sort_by_distance' is set to True.
+            Additional time complexity is introduced 
+            if 'sort_by_distance' is set to True.
         """
         if not isinstance(target, str):
             raise TypeError("The input parameter 'target' must be a string.")
@@ -53,11 +61,15 @@ class FuzzyTrie(Trie):
             raise TypeError("The input parameter 'sort_by_distance' must be a boolean.")
         
         if not isinstance(threshold, int) or threshold < 0:
-            raise ValueError("The input parameter 'threshold' must be a non-negative integer")
+            raise ValueError(
+                "The input parameter 'threshold' must be a non-negative integer"
+            )
         
         if (not isinstance(num_return, type(None)) and
             (not isinstance(num_return, int) or num_return < 1)):
-            raise ValueError("The input parameter 'num_return' must be a positive integer")
+            raise ValueError(
+                "The input parameter 'num_return' must be a positive integer"
+            )
         
         cols = len(target) + 1
         res = []
