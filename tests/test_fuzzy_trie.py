4cb20089aa73df47143efb3f6b512d442ec1748f
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-06-20 22:55:41 +0100

    update test for fuzzytrie

--- tests/test_fuzzy_trie.py
+++ tests/test_fuzzy_trie.py
@@ -4,4 +4,38 @@ from py_trie.fuzzy_trie import FuzzyTrie
 
 
 class TestFuzzyTrie(unittest.TestCase):
-    pass
+    """Test the FuzzyTrie properties."""
+
+    def setUp(self):
+        self.words = ['apple', 'abple', 'apples', 'apps', 'app']
+        self.trie = FuzzyTrie.from_list(self.words)
+
+    def test_fuzzy_search(self):
+        self.assertRaises(
+            TypeError, 
+            self.trie.fuzzy_search, None, 1, 1, True
+        )
+        self.assertRaises(
+            ValueError,
+            self.trie.fuzzy_search, 'a', -1, 1, True
+        )
+        self.assertRaises(
+            ValueError,
+            self.trie.fuzzy_search, 'a', 1, -1, True
+        )
+        self.assertRaises(
+            TypeError, 
+            self.trie.fuzzy_search, 'a', 1, 1, None
+        )
+        self.assertListEqual(
+            sorted(self.trie.fuzzy_search('apple', 1)),
+            ['abple', 'apple', 'apples']
+        )
+        self.assertListEqual(
+            sorted(self.trie.fuzzy_search('apple', 2)),
+            sorted(self.words)
+        )
+        self.assertIn('app', self.trie.fuzzy_search('app', 1))
+
+        
+        
